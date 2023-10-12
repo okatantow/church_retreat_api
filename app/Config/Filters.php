7ca0c2f100ea64033f21c5e293e74ecd9abc927f
okatantow <okatantow@gmail.com>
2023-10-12 12:18:20 +0000

    setup and completed jwt with other crud operations with authentication

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -8,6 +8,7 @@ use CodeIgniter\Filters\DebugToolbar;
 use CodeIgniter\Filters\Honeypot;
 use CodeIgniter\Filters\InvalidChars;
 use CodeIgniter\Filters\SecureHeaders;
+use App\Filters\JWTAuthenticationFilter;
 
 class Filters extends BaseConfig
 {
@@ -24,6 +25,7 @@ class Filters extends BaseConfig
         'honeypot'      => Honeypot::class,
         'invalidchars'  => InvalidChars::class,
         'secureheaders' => SecureHeaders::class,
+        'auth' => JWTAuthenticationFilter::class // add this line
     ];
 
     /**
@@ -66,5 +68,12 @@ class Filters extends BaseConfig
      * Example:
      * 'isLoggedIn' => ['before' => ['account/*', 'profiles/*']]
      */
-    public array $filters = [];
+    public array $filters = [
+        'auth' => [
+            'before' => [
+                'client/*',
+                'client'
+          ],
+        ]
+    ];
 }
--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,10 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->post('auth/register', 'Auth::register');
+$routes->post('auth/login', 'Auth::login');
+$routes->get('client', 'Client::index');
+$routes->post('client', 'Client::store');
+$routes->get('client/(:num)', 'Client::show/$1');
+$routes->post('client/(:num)', 'Client::update/$1');
+$routes->delete('client/(:num)', 'Client::destroy/$1');
