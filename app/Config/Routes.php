1fb92cc9969f9b12031bc06e83d21cfcf6f45d70
okatantow <okatantow@gmail.com>
2023-10-13 15:30:10 +0000

    implemented qr code generator

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -13,3 +13,12 @@ $routes->post('client', 'Client::store');
 $routes->get('client/(:num)', 'Client::show/$1');
 $routes->post('client/(:num)', 'Client::update/$1');
 $routes->delete('client/(:num)', 'Client::destroy/$1');
+
+/*
+ * --------------------------------------------------------------------
+ * Route Definitions
+ * --------------------------------------------------------------------
+ */
+
+ $routes->get('mail', 'SendMail::index');
+ $routes->match(['get', 'post'], 'SendMail/sendMail', 'SendMail::sendMail');
